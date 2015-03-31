6c9a28a073af4a74236ab06f66708e78528615b5
Jorge Novo <jnovos@gmail.com>
2015-03-31 19:55:08 +0200

    New version

--- lib/middleman-lunrjs/version.rb
+++ lib/middleman-lunrjs/version.rb
@@ -1,6 +1,6 @@
 module Middleman
   module Lunrjs
     PACKAGE = 'middleman-lunrjs'
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
   end
 end
--- make_gem.sh
+++ make_gem.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -f middleman-lunrjs-0.0.2.gem ]; then
+if [ -f middleman-lunrjs-0.0.3.gem ]; then
     echo "Delete File!"
-    rm middleman-lunrjs-0.0.2.gem
+    rm middleman-lunrjs-0.0.3.gem
     gem uninstall middleman-lunrjs
 fi
 gem build middleman-lunrjs.gemspec
-gem install middleman-lunrjs-0.0.2.gem --local
+gem install middleman-lunrjs-0.0.3.gem --local
