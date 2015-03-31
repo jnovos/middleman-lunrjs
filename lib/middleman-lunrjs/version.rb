173715c4356b1f062a9e2e40304589188ca7b2d0
Jorge Novo <jnovos@users.noreply.github.com>
2015-03-31 17:49:02 +0200

    Modify of https://github.com/everflux

--- lib/middleman-lunrjs/version.rb
+++ lib/middleman-lunrjs/version.rb
@@ -1,6 +1,6 @@
 module Middleman
   module Lunrjs
     PACKAGE = 'middleman-lunrjs'
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
--- make_gem.sh
+++ make_gem.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -f middleman-lunrjs-0.0.1.gem ]; then
+if [ -f middleman-lunrjs-0.0.2.gem ]; then
     echo "Delete File!"
-    rm middleman-lunrjs-0.0.1.gem
+    rm middleman-lunrjs-0.0.2.gem
     gem uninstall middleman-lunrjs
 fi
 gem build middleman-lunrjs.gemspec
-gem install middleman-lunrjs-0.0.1.gem --local
+gem install middleman-lunrjs-0.0.2.gem --local
